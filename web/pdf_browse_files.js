f80c3b283203eb40138358c2c884612de5ce6609
movsb <anhbk@qq.com>
2018-07-22 20:24:04 +0800

    eslint rules

--- web/pdf_browse_files.js
+++ web/pdf_browse_files.js
@@ -11,7 +11,7 @@ class PDFBrowseFiles {
 
     this.busy = false;
 
-    var self = this;
+    let self = this;
 
     if (closeButton) {
         closeButton.addEventListener('click', this.close.bind(this));
@@ -19,7 +19,7 @@ class PDFBrowseFiles {
     this.overlayManager.register(this.overlayName, this.container,
                                  this.close.bind(this));
 
-    this.container.querySelector('.wrapper').addEventListener('click', function(e){
+    this.container.querySelector('.wrapper').addEventListener('click', function(e) {
       if (self.busy) {
         console.log('busy');
         return;
@@ -28,94 +28,102 @@ class PDFBrowseFiles {
       self.onclick(e);
     });
   }
+
   open() {
     this.overlayManager.open(this.overlayName);
-    if(this.leftContainer.childElementCount == 0) {
+    if (this.leftContainer.childElementCount === 0) {
       this.load();
     }
   }
+
   close() {
       this.overlayManager.close(this.overlayName);
   }
+
   load() {
     this.readDir(this.leftContainer);
   }
+
   apiRoot() {
     // return 'http://127.0.0.1:8733/v1';
     return '/v1';
   }
+
   updateUI(element, base, files, folders) {
-    var self = this;
+    let self = this;
     element.innerHTML = '';
-    folders.forEach(name => {
-      var li = document.createElement('li');
+    folders.forEach((name) => {
+      let li = document.createElement('li');
       li.classList.add('folder-wrap');
-      var a = document.createElement('a');
+      let a = document.createElement('a');
       a.classList.add('folder');
       a.setAttribute('data-path', base + '/' + name);
       a.innerText = name;
       li.appendChild(a);
-      var subFolders = document.createElement('ul');
+      let subFolders = document.createElement('ul');
       subFolders.classList.add('folders');
       subFolders.setAttribute('data-path', base + '/' + name);
       li.appendChild(subFolders);
       element.appendChild(li);
     });
     self.rightContainer.innerHTML = '';
-    files.forEach(name=> {
-      var li = document.createElement('li');
+    files.forEach((name) => {
+      let li = document.createElement('li');
       li.classList.add('file-wrap');
-      var a = document.createElement('a');
+      let a = document.createElement('a');
       a.classList.add('file');
-      a.setAttribute('data-path',base + '/' + name);
+      a.setAttribute('data-path', base + '/' + name);
       a.innerText = name;
       li.appendChild(a);
       self.rightContainer.appendChild(li);
     });
   }
+
   onclick(e) {
     console.log(e.target);
     if (e.target.classList.contains('folders')) {
       this.readDir(e.target);
     } else if (e.target.classList.contains('folder')) {
-      var ul = e.target.nextSibling;
-      if (ul.style.display == "block" && ul.childElementCount > 0) {
-        ul.style.display = "none";
+      let ul = e.target.nextSibling;
+      if (ul.style.display === 'block' && ul.childElementCount > 0) {
+        ul.style.display = 'none';
         this.busy = false;
         return;
       }
-      ul.style.display = "block";
+      ul.style.display = 'block';
       this.readDir(ul);
-    } else if(e.target.classList.contains('file')) {
-      var path = decodeURIComponent(e.target.getAttribute('data-path'));
+    } else if (e.target.classList.contains('file')) {
+      let path = decodeURIComponent(e.target.getAttribute('data-path'));
       this.loadFile(path);
     } else {
       this.busy = false;
     }
   }
+
   readDir(element) {
-    var self = this;
-    var base = element.getAttribute('data-path');
-    var xhr = new XMLHttpRequest();
+    let self = this;
+    let base = element.getAttribute('data-path');
+    let xhr = new XMLHttpRequest();
     xhr.onload = function() {
         console.log(xhr.response);
-        var d = JSON.parse(xhr.response);
+        let d = JSON.parse(xhr.response);
         self.updateUI(element, base, d.files, d.folders);
         self.busy = false;
-    }
+    };
     xhr.open('GET', self.apiRoot() + '/list?base=' + encodeURIComponent(base));
     xhr.send();
   }
+
   loadFile(path) {
-    var self = this;
-    var url = this.apiRoot() + '/file?file=' + encodeURIComponent(path);
+    let self = this;
+    let url = this.apiRoot() + '/file?file=' + encodeURIComponent(path);
     PDFViewerApplication.open(url)
-    .then(function(){
+    .then(function() {
       self.close();
       self.busy = false;
     })
-    .catch(function(){
-      alert("Failed to open file.");
+    .catch(function() {
+      alert('Failed to open file.');
       self.busy = false;
     })
     ;
